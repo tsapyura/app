130e536fcb2a0b3ee8c2d65e6950248a32927fd8
Yura Tsap <tsapyura1999@gmail.com>
2022-09-28 15:41:41 +0300

    add some fix

--- config/settings.py
+++ config/settings.py
@@ -88,12 +88,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres",
-        "PASSWORD": "tsap",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "PASSWORD": "postgres",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
     }
 }
 # Password validation
